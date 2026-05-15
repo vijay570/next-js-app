8a4b89337669a43e3b18dab5ee95379a7469a3f6
vijay570 <57717201+vijay570@users.noreply.github.com>
2026-05-15 16:36:22 +0530

    static

--- app/(application)/blogs/[blogID]/page.js
+++ app/(application)/blogs/[blogID]/page.js
@@ -1,16 +1,55 @@
-import { notFound } from "next/navigation";
+import Link from "next/link";
 
-export async function generateMetadata({ params }) {
-  const { blogID } = await params;
-  return {
-    title: `Blog ${blogID}`,
-  };
-}
 
-export default async function Blog({ params }) {
+// export async function generateStaticParams() {
+//   const response = await fetch("https://jsonplaceholder.typicode.com/todos");
+//   const data = await response.json();
+//   console.log(data);
+  // return data.map(({ id }) => ({ blogID: `${id}` }));
+  // return [
+  //   { blogID: "1" },
+  //   { blogID: "2" },
+  //   { blogID: "3" },
+  //   { blogID: "4" },
+  //   { blogID: "5" },
+  //   { blogID: "6" },];
+// }
+
+const Blog = async ({ params }) => {
   const { blogID } = await params;
-  if (!/^\d+$/.test(blogID)) {
-    notFound();
-  }
-  return <div>Blog {blogID}</div>;
-}
+  console.log("blogID: ", blogID);
+  return (
+    <>
+      <nav>
+        <ul className="navbar">
+          <li>
+            <Link href="/" className="nav-link">
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link href="/about" className="nav-link">
+              About
+            </Link>
+          </li>
+          <li>
+            <Link href="/services" className="nav-link">
+              Services
+            </Link>
+          </li>
+          <li>
+            <Link href="/blogs" className="nav-link active">
+              Blogs
+            </Link>
+          </li>
+        </ul>
+      </nav>
+      <div>
+        <h1>Welcome to Our Blog {blogID}</h1>
+        <p>This is blog {blogID} page.</p>
+      </div>
+    </>
+  );
+};
+
+export default Blog;
--- app/(application)/blogs/page.js
+++ app/(application)/blogs/page.js
@@ -1,14 +1,49 @@
-export const metadata = {
-  title: "Blogs",
-};
+import Link from "next/link";
 
-export default async function Blogs() {
+const Blogs = () => {
+  console.log("Blogs Page");
   return (
     <>
-      <h1>All Blogs</h1>
-      <p>Blog 1</p>
-      <p>Blog 2</p>
-      <p>Blog 3</p>
+      <nav>
+        <ul className="navbar">
+          <li>
+            <Link href="/" className="nav-link">
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link href="/about" className="nav-link">
+              About
+            </Link>
+          </li>
+          <li>
+            <Link href="/services" className="nav-link">
+              Services
+            </Link>
+          </li>
+          <li>
+            <Link href="/blogs" className="nav-link active">
+              Blogs
+            </Link>
+          </li>
+        </ul>
+      </nav>
+      <div>
+        <h1>Welcome to Our Blog</h1>
+        <ol className="blog-links">
+          <li>
+            <Link href="/blogs/1">Blog 1</Link>
+          </li>
+          <li>
+            <Link href="/blogs/2">Blog 2</Link>
+          </li>
+          <li>
+            <Link href="/blogs/3">Blog 3</Link>
+          </li>
+        </ol>
+      </div>
     </>
   );
-}
+};
+
+export default Blogs;
